8a19eb3d38068d7b4c13cdbbf9fe76e38bc5eba4
LeonardoRT96 <32780692+LeonardoRT96@users.noreply.github.com>
2017-11-16 01:18:44 -0600

    Update Welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -156,6 +156,172 @@ class Welcome extends CI_Controller {
 			show_error($e->getMessage().' --- '.$e->getTraceAsString());
 		}
 	}
+	public function individual(){
+		try{
+			$crud = new grocery_crud();
+			$crud->set_table('persona_has_categoria');
+			$crud->columns('id_Persona','id_Categoria','id_Competencia');
+			$crud->set_theme('datatables');
+			$crud->required_fields('id_Persona','id_Categoria','id_Competencia');
+			$this->load->view('head');
+			$output = $crud->render();
+			$this->_example_output($output);
+		}catch(Exception $e){
+			show_error($e->getMessage().' --- '.$e->getTraceAsString());
+		}
+	}
+	public function insertaEquipo(){
+		$data['data'] = array(
+			'nombre' => $this->input->post('nombre'),
+			'dojo' => $this->input->post('dojo')
+		);      
+		//echo var_dump($data['data']);
+		$this->bases_model->insertaEquipo($data['data']);	
+		redirect('/Welcome/registro/', 'refresh');
+	}
+	public function insertaEC(){
+		$data['data'] = array(
+			'equipo' => $this->input->post('equipo'),
+			'competencia' => $this->input->post('competencia'),
+			'categoria' => $this->input->post('categoria1')
+		);   
+		echo var_dump($data['data']);
+		$this->bases_model->insertaEC($data['data']);	
+		redirect('/Welcome/equipoCategoria/', 'refresh');
+	}
 
+	public function consultas(){
+		$this->load->view('head');
+		$this->load->view('header');
+		$this->load->view('encabezado');
+		$cadena = "select * from competencia where id_Competencia<3";
+		$data['competenciaI']=$this->bases_model->getQuery($cadena);
+		$cadena = "select * from competencia where id_Competencia>2";
+		$data['competenciaE']=$this->bases_model->getQuery($cadena);
+		$this->load->view('consultas',$data);
+		$this->load->view('footer');
+	}
+
+	public function llenaCategoriaI(){//esta funcion llena el div de la consulta
+		$options = "";
+      	if($this->input->post('competenciaI')){
+      		$competencia = $this->input->post('competenciaI');
+      		$cadena="select * from categoria where id_Competencia='".$competencia."'";
+       		$categoria = $this->bases_model->getQuery($cadena);?>
+         	<div name="categoriaI" id="categoriaI">
+          		<label for="categoriaI">Selecciona una Categoria:</label>
+             	<select class="form-control" id="categoriaI1" name="categoriaI1">
+             		<option value="" disabled selected>click aquí</option>
+             		<?php foreach($categoria->result() as $fila){?>
+             			<option value="<?=$fila->id_Categoria?>"><?=$fila ->nombre_categoria?></option> 
+             		<?php }?>
+             	</select>
+          	</div>
+         	<?php 
+       	}
+	}
+	public function llenaCategoriaE(){//esta funcion llena el div de la consulta
+		$options = "";
+      	if($this->input->post('competenciaE')){
+      		$competencia = $this->input->post('competenciaE');
+      		$cadena="select * from categoria where id_Competencia='".$competencia."'";
+       		$categoria = $this->bases_model->getQuery($cadena);?>
+         	<div name="categoriaE" id="categoriaE">
+          		<label for="categoriaE">Selecciona una Categoria:</label>
+             	<select class="form-control" id="categoriaE1" name="categoriaE1">
+             		<option value="" disabled selected>click aquí</option>
+             		<?php foreach($categoria->result() as $fila){?>
+             			<option value="<?=$fila->id_Categoria?>"><?=$fila ->nombre_categoria?></option> 
+             		<?php }?>
+             	</select>
+          	</div>
+         	<?php 
+       	}
+	}
+	public function llenaCompetidoresI(){//esta funcion da la consulta
+		
+		$data['data'] = array(
+			'categoria' => $this->input->post('categoriaI1'),
+			'competencia' => $this->input->post('competenciaI')
+		);      
+		//echo var_dump($data['data']);
+		
+       	$competidores = $this->bases_model->getCompetidores($data['data']);
+		
+		
+		if ($competidores!=FALSE){ 
+			$i=1;
+			foreach($competidores->result() as $row) { ?>
+			  <form id="form<?php echo $i;?>" name="form<?php echo $i;?>" class="form-inline alert alert-info">
+			    <div class="form-group">
+			     <label for="nombre_persona<?php echo $i;?>">nombre:</label>
+			     <input type="text" class="form-control" readonly id="nombre_persona<?php echo $i;?>" name="nombre_persona<?php echo $i;?>" value="<?php echo $row->nombre_persona;?>">
+			     <label for="dojo<?php echo $i;?>">dojo:</label>
+			     <input type="text" class="form-control" readonly id="dojo<?php echo $i;?>" name="dojo<?php echo $i;?>" value="<?php echo $row->nombre_dojo;?>">
+			     <label for="edad<?php echo $i;?>">edad:</label>
+			     <input type="text" class="form-control" readonly id="edad<?php echo $i;?>" name="edad<?php echo $i;?>" value="<?php echo $row->edad;?>">
+			    </div>			    
+			  </form>
+			  <?php  $i++;}
+		}
+    }
+    public function llenaCompetidoresE(){//esta funcion da la consulta
+		
+		$data['data'] = array(
+			'categoria' => $this->input->post('categoriaE1'),
+			'competencia' => $this->input->post('competenciaE')
+		);      
+		//echo var_dump($data['data']);
+		
+       	$competidores = $this->bases_model->getCompetidoresE($data['data']);
+		
+		
+		if ($competidores!=FALSE){ 
+			$i=1;
+			foreach($competidores->result() as $row) { ?>
+			  <form id="form<?php echo $i;?>" name="form<?php echo $i;?>" class="form-inline alert alert-info">
+			    <div class="form-group">
+			     <label for="id_Equipo<?php echo $i;?>">nombre:</label>
+			     <input type="text" class="form-control" readonly id="nombre_equipo<?php echo $i;?>" name="nombre_equipo<?php echo $i;?>" value="<?php echo $row->nombre_equipo;?>">
+			     <label for="dojo<?php echo $i;?>">dojo:</label>
+			     <input type="text" class="form-control" readonly id="dojo<?php echo $i;?>" name="dojo<?php echo $i;?>" value="<?php echo $row->nombre_dojo;?>">
+			    </div>
+			  </form>
+			  <?php  $i++;}
+		}
+    }
+	
+	public function llenaCategorias(){//esta funcion solo es para dar de alta en el registro
+      	$options = "";
+      	if($this->input->post('competencia')){
+      		$competencia = $this->input->post('competencia');
+      		$cadena="select * from categoria where id_Competencia='".$competencia."'";
+       		$categoria = $this->bases_model->getQuery($cadena);?>
+         	<div name="categoria" id="categoria">
+          		<label for="categoria">Selecciona una Categoria:</label>
+             	<select class="form-control" id="categoria1" name="categoria1">
+             		<?php foreach($categoria->result() as $fila){?>
+             			<option value="<?=$fila->id_Categoria?>"><?=$fila ->nombre_categoria?></option> 
+             		<?php }?>
+             	</select>
+          	</div>
+         	<?php 
+       	}
+    }
+
+
+	public function equipoCategoria(){
+		$this->load->view('header');
+		$cadena = "select * from equipo where id_equipo>76 order by id_Equipo";
+		$data['equipos']=$this->bases_model->getQuery($cadena);
+		$cadena = "select * from competencia order by id_Competencia";
+		$data['competencias']=$this->bases_model->getQuery($cadena);
+		$this->load->view('equipoCategoria',$data);
+	}
+	public function registro(){
+		$cadena = "select * from dojo order by nombre_dojo";
+		$data['dojos']=$this->bases_model->getQuery($cadena);
+		$this->load->view('registro',$data);
+	}
 
 }
